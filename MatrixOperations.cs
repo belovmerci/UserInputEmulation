9cc0f61ade08e4b77080b9db9b0b3ce47fd3dfff
Arty <belovaa@mer.ci.nsu.ru>
2024-05-19 19:58:47 +0700

    Initial commit

---
+++ MatrixOperations.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInputEmulation
+{
+    public static class MatrixOperations
+    {
+        public static int[,] CreateMultiplicationMatrix(int x, int y)
+        {
+            int[,] matrix = new int[y, x];
+
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    matrix[i, j] = (i + 1) * (j + 1);
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void DisplayMatrix(int[,] matrix, int x, int y)
+        {
+            Console.WriteLine("[");
+            for (int i = 0; i < y; i++)
+            {
+                Console.Write("[");
+
+                for (int j = 0; j < x; j++)
+                {
+                    if (j < x - 1)
+                    {
+                        Console.Write(matrix[i, j] + ", ");
+                    }
+                    else
+                    {
+                        Console.Write(matrix[i, j]);
+                    }
+                }
+
+                if (i < y - 1)
+                {
+                    Console.WriteLine("],");
+                }
+                else
+                {
+                    Console.WriteLine("]");
+                }
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInputEmulation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool isTest = args.Length > 0 && args[0] == "test";
+            int x, y;
+
+            Console.WriteLine("Введите два числа (x и y):");
+
+            // Input for x
+            while (true)
+            {
+                Console.Write("Введите число x: ");
+                if (int.TryParse(Console.ReadLine(), out x) && x > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Пожалуйста, введите положительное целое число.");
+                }
+            }
+
+            // Input for y
+            while (true)
+            {
+                Console.Write("Введите число y: ");
+                if (int.TryParse(Console.ReadLine(), out y) && y > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Пожалуйста, введите положительное целое число.");
+                }
+            }
+
+            // Create and display the multiplication matrix
+            int[,] matrix = MatrixOperations.CreateMultiplicationMatrix(x, y);
+            MatrixOperations.DisplayMatrix(matrix, x, y);
+
+            // Wait to show output if is running in test mode
+            if (!isTest) Console.ReadKey();
+        }
+    }
+}
